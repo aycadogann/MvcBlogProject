b79e501c74597ab6264c6cdee4b102c49cfe77e2
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-02 20:30:32 +0300

    Add project files.

---
+++ MvcBlogProject.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBlogProject.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
---
+++ MvcBlogProject.EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBlogProject.EntityLayer.Concrete
+{
+    public class Heading
+    {
+        public int HeadingID { get; set; }
+        public string HeadName { get; set; }
+        public DateTime HeadingDate { get; set; }
+    }
+}
