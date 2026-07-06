b64dcf585217b96cc780cae222e7f16cab15133b
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-06 17:27:59 +0300

    generic IRepository interface olustur ve writer dataaccess interface olustur

---
+++ MvcBlogProject.DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBlogProject.DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
+
+
--- MvcBlogProject.DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
+++ MvcBlogProject.DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -1,13 +1,38 @@
 ﻿using MvcBlogProject.DataAccessLayer.Abstract;
+using MvcBlogProject.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MvcBlogProject.DataAccessLayer.Concrete.Repositories
 {
-    public class CategoryRepository:ICategoryDal
+    public class CategoryRepository : ICategoryDal
     {
+        Context context = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category category)
+        {
+            _object.Remove(category);
+            context.SaveChanges();
+        }
+
+        public List<Category> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public void Insert(Category category)
+        {
+            _object.Add(category);
+            context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            context.SaveChanges();
+        }
     }
 }
