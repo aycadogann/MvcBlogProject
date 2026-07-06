5b7a3c0af3bdf564c745ba4fc6ac88ace2988c1a
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-06 18:20:57 +0300

    categorymanager sinifini ve business kodlarini yaz

---
+++ MvcBlogProject.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using MvcBlogProject.DataAccessLayer.Concrete.Repositories;
+using MvcBlogProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBlogProject.BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+        public List<Category> GetAllBL()
+        {
+            return repository.GetAll();
+        }
+        public void AddBL(Category category)
+        {
+            if (category.CategoryName=="" || category.CategoryName.Length<=3 || 
+                category.CategoryDescription=="" || category.CategoryName.Length>=51)
+            {
+                //Hata mesajı
+            }
+            else
+            {
+                repository.Insert(category);
+            }
+        }
+    }
+}
