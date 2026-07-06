f9cbee7acd5b1dd9ca5c7fb91bd4371b3a73a1fe
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-06 16:36:19 +0300

    category icin dataaccess interface olustur

---
+++ MvcBlogProject.DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using MvcBlogProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBlogProject.DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> GetAll();
+        void Insert(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+    }
+}
