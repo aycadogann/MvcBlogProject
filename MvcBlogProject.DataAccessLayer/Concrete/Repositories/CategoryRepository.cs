326658afdeb1902a4e38bbf74ba92f31001b194d
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-06 16:39:50 +0300

    category dataaccess concrete repository sinifini olustur

---
+++ MvcBlogProject.DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using MvcBlogProject.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcBlogProject.DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository:ICategoryDal
+    {
+    }
+}
