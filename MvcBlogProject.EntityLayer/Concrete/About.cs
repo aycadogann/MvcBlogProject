c2839acc5128d983f7b4d1b13428d359f43c256a
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-06 14:57:45 +0300

    Entity framework nuget paketlerini yükle

--- MvcBlogProject.EntityLayer/Concrete/About.cs
+++ MvcBlogProject.EntityLayer/Concrete/About.cs
@@ -8,6 +8,7 @@ namespace MvcBlogProject.EntityLayer.Concrete
 {
     public class About
     {
+
         public int AboutID { get; set; }
         public string AboutDetails1 { get; set; }
         public string AboutDetails2 { get; set; }
