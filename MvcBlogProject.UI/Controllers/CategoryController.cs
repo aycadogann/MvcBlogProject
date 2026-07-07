cb5422c5ad0404a8b6026187313c2c4d258a3897
Ayça Doğan <dogan.aycaa@gmail.com>
2026-07-07 10:49:22 +0300

    business katmani icin tum abstract tanimlamalarini yap

--- MvcBlogProject.UI/Controllers/CategoryController.cs
+++ MvcBlogProject.UI/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcBlogProject.BusinessLayer.Concrete;
+using MvcBlogProject.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,29 @@ namespace MvcBlogProject.UI.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager categoryManager = new CategoryManager();
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAllBL();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            categoryManager.AddBL(category);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
